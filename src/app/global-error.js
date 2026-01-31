6aaa48a551bb2265c4683cf6576f7e47c60f8093
Richard Lefevre <rlefevre.6505@gmail.com>
2026-01-31 18:02:08 +0000

    feat: added global error and no-found pages

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -1,13 +1,18 @@
 "use client";
 import Link from "next/link";
+import styles from "./global-error.module.css";
 
 export default function GlobalError({ error, reset }) {
   return (
     // global-error must include html and body tags
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <div className={styles.main}>
+          <h2 className={styles.h2}>Something went wrong!</h2>
+          <Link className={styles.link} href="/">
+            Return to GigLog home page
+          </Link>
+        </div>
       </body>
     </html>
   );
